ce0e4da4260bf48dc08447d1f5e3d4a778813335
rickying <ing.rickyy@gmail.com>
2020-07-30 01:00:40 -0700

    histogram changes

--- consensus/service/src/counters.rs
+++ consensus/service/src/counters.rs
@@ -14,7 +14,7 @@ lazy_static::lazy_static! {
     pub static ref PENDING_VALUE_PROCESSING_TIME: Histogram = register_histogram!(
         "pending_value_processing_time",
         "Time from receiving a value until it is externalized (in seconds)",
-        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 15.0, 20.0, 25.0, 30.0, 45.0, 60.0]
+        vec![.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 2.0, 3.0, 4.0, 5.0]
     ).unwrap();
 }
 
